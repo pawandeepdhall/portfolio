ae78c8e1dfc9463b7086f62ed214727cbc398bdb
aryanjangid <aryanjangid2511@gmail.com>
2022-06-28 08:01:53 +0530

    color change

--- vcard-personal-portfolio-master/assets/js/script.js
+++ vcard-personal-portfolio-master/assets/js/script.js
@@ -140,25 +140,6 @@ window.onclick = (event) => {
   }
 };
 // contact form variables
-const form = document.querySelector("[data-form]");
-const formInputs = document.querySelectorAll("[data-form-input]");
-const formBtn = document.querySelector("[data-form-btn]");
-
-// add event to all form input field
-for (let i = 0; i < formInputs.length; i++) {
-  formInputs[i].addEventListener("input", function () {
-
-    // check form validation
-    if (form.checkValidity()) {
-      formBtn.removeAttribute("disabled");
-    } else {
-      formBtn.setAttribute("disabled", "");
-    }
-
-  });
-}
-
-
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
@@ -180,4 +161,141 @@ for (let i = 0; i < navigationLinks.length; i++) {
     }
 
   });
+}
+
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Thanks for your submission!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
+
+    document.querySelectorAll('.abutton').forEach(button => {
+
+      let duration = 3000,
+          svg = button.querySelector('svg'),
+          svgPath = new Proxy({
+              y: null,
+              smoothing: null
+          }, {
+              set(target, key, value) {
+                  target[key] = value;
+                  if(target.y !== null && target.smoothing !== null) {
+                      svg.innerHTML = getPath(target.y, target.smoothing, null);
+                  }
+                  return true;
+              },
+              get(target, key) {
+                  return target[key];
+              }
+          });
+  
+      button.style.setProperty('--duration', duration);
+  
+      svgPath.y = 20;
+      svgPath.smoothing = 0;
+  
+      button.addEventListener('click', e => {
+          
+          e.preventDefault();
+  
+          if(!button.classList.contains('loading')) {
+  
+              button.classList.add('loading');
+  
+              gsap.to(svgPath, {
+                  smoothing: .3,
+                  duration: duration * .065 / 1000
+              });
+  
+              gsap.to(svgPath, {
+                  y: 12,
+                  duration: duration * .265 / 1000,
+                  delay: duration * .065 / 1000,
+                  ease: Elastic.easeOut.config(1.12, .4)
+              });
+  
+              setTimeout(() => {
+                  svg.innerHTML = getPath(0, 0, [
+                      [3, 14],
+                      [8, 19],
+                      [21, 6]
+                  ]);
+              }, duration / 2);
+  
+          }
+  
+      });
+  
+  });
+  
+  function getPoint(point, i, a, smoothing) {
+      let cp = (current, previous, next, reverse) => {
+              let p = previous || current,
+                  n = next || current,
+                  o = {
+                      length: Math.sqrt(Math.pow(n[0] - p[0], 2) + Math.pow(n[1] - p[1], 2)),
+                      angle: Math.atan2(n[1] - p[1], n[0] - p[0])
+                  },
+                  angle = o.angle + (reverse ? Math.PI : 0),
+                  length = o.length * smoothing;
+              return [current[0] + Math.cos(angle) * length, current[1] + Math.sin(angle) * length];
+          },
+          cps = cp(a[i - 1], a[i - 2], point, false),
+          cpe = cp(point, a[i - 1], a[i + 1], true);
+      return `C ${cps[0]},${cps[1]} ${cpe[0]},${cpe[1]} ${point[0]},${point[1]}`;
+  }
+  
+  function getPath(update, smoothing, pointsNew) {
+      let points = pointsNew ? pointsNew : [
+              [4, 12],
+              [12, update],
+              [20, 12]
+          ],
+          d = points.reduce((acc, point, i, a) => i === 0 ? `M ${point[0]},${point[1]}` : `${acc} ${getPoint(point, i, a, smoothing)}`, '');
+      return `<path d="${d}" />`;
+  }
+  
+  var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
